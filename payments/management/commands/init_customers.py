cda1575661302af30fb7d810ae0ca39ea254e2df
Patrick Altman <paltman@gmail.com>
2012-08-19 17:53:09 -0500

    Add a callback to facilitate site definition of per user trial_end setting

--- payments/management/commands/init_customers.py
+++ payments/management/commands/init_customers.py
@@ -3,6 +3,8 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
 from payments.models import Customer
+from payments.settings import TRIAL_PERIOD_FOR_USER_CALLBACK
+from payments.settings import DEFAULT_PLAN
 
 
 class Command(BaseCommand):
@@ -11,5 +13,10 @@ class Command(BaseCommand):
     
     def handle(self, *args, **options):
         for user in User.objects.filter(customer__isnull=True):
-            Customer.create(user=user)
+            trial_days = None
+            if TRIAL_PERIOD_FOR_USER_CALLBACK:
+                trial_days = TRIAL_PERIOD_FOR_USER_CALLBACK(user)
+            cus = Customer.create(user=user)
+            if DEFAULT_PLAN:
+                cus.purchase(plan=DEFAULT_PLAN, trial_days=trial_days)
             print "Created customer for %s" % user.email
--- payments/settings.py
+++ payments/settings.py
@@ -1,4 +1,27 @@
+import importlib
+
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+
+def plan_from_stripe_id(stripe_id):
+    for key in PAYMENTS_PLANS.keys():
+        if PAYMENTS_PLANS[key].get("stripe_plan_id") == stripe_id:
+            return key
+
+
+def load_path_attr(path):
+    i = path.rfind(".")
+    module, attr = path[:i], path[i + 1:]
+    try:
+        mod = importlib.import_module(module)
+    except ImportError, e:
+        raise ImproperlyConfigured("Error importing %s: '%s'" % (module, e))
+    try:
+        attr = getattr(mod, attr)
+    except AttributeError:
+        raise ImproperlyConfigured("Module '%s' does not define a '%s'" % (module, attr))
+    return attr
 
 
 INVOICE_FROM_EMAIL = getattr(
@@ -11,9 +34,17 @@ PLAN_CHOICES = [
     (key, settings.PAYMENTS_PLANS[key].get("name", key))
     for key in settings.PAYMENTS_PLANS
 ]
-
-
-def plan_from_stripe_id(stripe_id):
-    for key in PAYMENTS_PLANS.keys():
-        if PAYMENTS_PLANS[key].get("stripe_plan_id") == stripe_id:
-            return key
+DEFAULT_PLAN = getattr(
+    settings,
+    "PAYMENTS_DEFAULT_PLAN",
+    None
+)
+TRIAL_PERIOD_FOR_USER_CALLBACK = getattr(
+    settings,
+    "PAYMENTS_TRIAL_PERIOD_FOR_USER_CALLBACK",
+    None
+)
+if isinstance(TRIAL_PERIOD_FOR_USER_CALLBACK, basestring):
+    TRIAL_PERIOD_FOR_USER_CALLBACK = load_path_attr(
+        TRIAL_PERIOD_FOR_USER_CALLBACK
+    )

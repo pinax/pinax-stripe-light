f2b822345c8996df6d000cc0b1392ada010ee367
Kenneth Love <kenneth@gigantuan.net>
2012-12-31 15:56:52 -0800

    Update payments/management/commands/init_customers.py

--- payments/management/commands/init_customers.py
+++ payments/management/commands/init_customers.py
@@ -1,6 +1,5 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
-
-from django.contrib.auth.models import User
 
 from payments.models import Customer
 from payments.settings import TRIAL_PERIOD_FOR_USER_CALLBACK
@@ -12,7 +11,7 @@ class Command(BaseCommand):
     help = "Create customer objects for existing users that don't have one"
     
     def handle(self, *args, **options):
-        for user in User.objects.filter(customer__isnull=True):
+        for user in settings.AUTH_USER_MODEL.objects.filter(customer__isnull=True):
             trial_days = None
             if TRIAL_PERIOD_FOR_USER_CALLBACK:
                 trial_days = TRIAL_PERIOD_FOR_USER_CALLBACK(user)

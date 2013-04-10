c97bae4fe5f0df1fcb6dc9636f67d274a63edc6b
Patrick Altman <paltman@gmail.com>
2013-04-10 13:45:18 -0500

    Fix deprecation warning re: urls.defaults
    Thanks, Fred Palmer (https://github.com/fredpalmer)

--- payments/urls.py
+++ payments/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 from django.contrib.auth.decorators import login_required

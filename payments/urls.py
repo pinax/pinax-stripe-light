e5b8b901673d347550db632764ac0050d352058c
Brent O'Connor <epicserve@gmail.com>
2013-04-11 12:56:33 -0700

    More PEP8 fixes

--- payments/urls.py
+++ payments/urls.py
@@ -1,18 +1,19 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from payments.views import SubscribeView, ChangeCardView, ChangePlanView, CancelView, HistoryView
+from payments.views import (SubscribeView, ChangeCardView, ChangePlanView,
+                            CancelView, HistoryView)
 
 
 urlpatterns = patterns(
     "payments.views",
     url(r"^webhook/$", "webhook", name="payments_webhook"),
-    
+
     url(r"^a/subscribe/$", "subscribe", name="payments_ajax_subscribe"),
     url(r"^a/change/card/$", "change_card", name="payments_ajax_change_card"),
     url(r"^a/change/plan/$", "change_plan", name="payments_ajax_change_plan"),
     url(r"^a/cancel/$", "cancel", name="payments_ajax_cancel"),
-    
+
     url(
         r"^subscribe/$",
         login_required(SubscribeView.as_view()),

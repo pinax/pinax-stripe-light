b611e2d5d833d15e29b57a06f14240dccfd52b7a
Ted Chen <surfer26th@yahoo.com>
2013-04-30 11:29:23 -0700

    Update forms.py

--- payments/forms.py
+++ payments/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 from payments.settings import PLAN_CHOICES
-
+sorted_plan_choices = sorted(PLAN_CHOICES)
 
 class PlanForm(forms.Form):
     
-    plan = forms.ChoiceField(choices=PLAN_CHOICES + [("", "-------")])
+    plan = forms.ChoiceField(choices=sorted_plan_choices + [("", "-------")])

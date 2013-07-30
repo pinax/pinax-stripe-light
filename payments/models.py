6db9375cf1bcdf15f48f1f3f18340ebc0917e5ab
Daniel Greenfeld <pydanny@gmail.com>
2013-07-31 00:39:40 +0200

    small compat fixes

--- payments/models.py
+++ payments/models.py
@@ -75,7 +75,7 @@ class EventProcessingException(models.Model):
         )
     
     def __unicode__(self):
-        return u"<%s, pk=%s, Event=%s>" % (self.message, self.pk, self.event)
+        return "<%s, pk=%s, Event=%s>" % (self.message, self.pk, self.event)
     
     def __str__(self):
         return self.__unicode__()

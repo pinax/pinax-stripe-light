af24e268d1499ff3d54c615f6b5760d502ae46e8
Patrick Altman <paltman@gmail.com>
2013-08-18 16:26:50 -0500

    Fix bug in middleware that occurs when a user does not have a customer

--- payments/middleware.py
+++ payments/middleware.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 
+from .models import Customer
+
 
 URLS = [reverse(url) for url in settings.SUBSCRIPTION_REQUIRED_EXCEPTION_URLS]
 
@@ -11,5 +13,8 @@ class ActiveSubscriptionMiddleware(object):
     def process_request(self, request):
         if request.user.is_authenticated() and not request.user.is_staff:
             if request.path not in URLS:
-                if not request.user.customer.has_active_subscription():
+                try:
+                    if not request.user.customer.has_active_subscription():
+                        return redirect(settings.SUBSCRIPTION_REQUIRED_REDIRECT)
+                except Customer.DoesNotExist:
                     return redirect(settings.SUBSCRIPTION_REQUIRED_REDIRECT)

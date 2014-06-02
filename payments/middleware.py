72da9c0e5a68259d86015e5aec4e6ea2c3b4747c
Andrew Pryde <andrew@rocketpod.co.uk>
2014-06-02 12:58:53 +0100

    Patched ActiveSubscriptionMiddleware to support urls containing patterns

--- payments/middleware.py
+++ payments/middleware.py
@@ -1,20 +1,20 @@
 from django.conf import settings
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import resolve
 from django.shortcuts import redirect
 
 from .models import Customer
-
-
-URLS = [reverse(url) for url in settings.SUBSCRIPTION_REQUIRED_EXCEPTION_URLS]
 
 
 class ActiveSubscriptionMiddleware(object):
 
     def process_request(self, request):
         if request.user.is_authenticated() and not request.user.is_staff:
-            if request.path not in URLS:
+            url_name = resolve(request.path).url_name
+            if url_name not in settings.SUBSCRIPTION_REQUIRED_EXCEPTION_URLS:
                 try:
                     if not request.user.customer.has_active_subscription():
-                        return redirect(settings.SUBSCRIPTION_REQUIRED_REDIRECT)
+                        return redirect(
+                            settings.SUBSCRIPTION_REQUIRED_REDIRECT
+                        )
                 except Customer.DoesNotExist:
                     return redirect(settings.SUBSCRIPTION_REQUIRED_REDIRECT)
---
+++ payments/tests/test_urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, url
+from mock_views import MockView
+from ..urls import urlpatterns
+
+urlpatterns += patterns(
+    '',
+    url(
+        r'^the/app/$',
+        MockView.as_view(),
+        name='the_app'
+    ),
+    url(
+        r'^accounts/signup/$',
+        MockView.as_view(),
+        name='signup'
+    ),
+    url(
+        r'^password/reset/confirm/(?P<token>.+)/$',
+        MockView.as_view(),
+        name='password_reset'
+    ),
+)

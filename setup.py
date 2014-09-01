678ba202562c19c9edc5d812b8f8fcb5730d9ed8
Domen Kožar <domen@dev.si>
2014-09-01 15:58:37 +0200

    Add PyPi classifiers

--- setup.py
+++ setup.py
@@ -129,6 +129,10 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Framework :: Django",
     ],
     install_requires=[

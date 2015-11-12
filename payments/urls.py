1975aeb06a85d8983a3815ffd89076af66d61561
Patrick Altman <paltman@gmail.com>
2015-11-11 20:10:01 -0500

    Use imported views instead of lazy import

--- payments/urls.py
+++ payments/urls.py
@@ -10,17 +10,22 @@ from .views import (
     ChangeCardView,
     ChangePlanView,
     HistoryView,
-    SubscribeView
+    SubscribeView,
+    webhook,
+    subscribe,
+    change_card,
+    change_plan,
+    cancel
 )
 
 
 urlpatterns = patterns(
-    "payments.views",
-    url(r"^webhook/$", "webhook", name="payments_webhook"),
-    url(r"^a/subscribe/$", "subscribe", name="payments_ajax_subscribe"),
-    url(r"^a/change/card/$", "change_card", name="payments_ajax_change_card"),
-    url(r"^a/change/plan/$", "change_plan", name="payments_ajax_change_plan"),
-    url(r"^a/cancel/$", "cancel", name="payments_ajax_cancel"),
+    "",
+    url(r"^webhook/$", webhook, name="payments_webhook"),
+    url(r"^a/subscribe/$", subscribe, name="payments_ajax_subscribe"),
+    url(r"^a/change/card/$", change_card, name="payments_ajax_change_card"),
+    url(r"^a/change/plan/$", change_plan, name="payments_ajax_change_plan"),
+    url(r"^a/cancel/$", cancel, name="payments_ajax_cancel"),
     url(
         r"^subscribe/$",
         login_required(SubscribeView.as_view()),

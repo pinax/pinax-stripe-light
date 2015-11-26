05d36bbe3ee313a138b7364c64640c866fd2adde
Patrick Altman <paltman@gmail.com>
2015-11-26 00:50:55 -0600

    ObjectNotExists is not possible anymore

--- pinax/stripe/middleware.py
+++ pinax/stripe/middleware.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import resolve
 from django.shortcuts import redirect
 
@@ -12,11 +11,8 @@ class ActiveSubscriptionMiddleware(object):
         if request.user.is_authenticated() and not request.user.is_staff:
             url_name = resolve(request.path).url_name
             if url_name not in settings.PINAX_STRIPE_SUBSCRIPTION_REQUIRED_EXCEPTION_URLS:
-                try:
-                    customer = customers.get_customer_for_user(request.user)
-                    if not subscriptions.has_active_subscription(customer):
-                        return redirect(
-                            settings.PINAX_STRIPE_SUBSCRIPTION_REQUIRED_REDIRECT
-                        )
-                except ObjectDoesNotExist:
-                    return redirect(settings.PINAX_STRIPE_SUBSCRIPTION_REQUIRED_REDIRECT)
+                customer = customers.get_customer_for_user(request.user)
+                if not subscriptions.has_active_subscription(customer):
+                    return redirect(
+                        settings.PINAX_STRIPE_SUBSCRIPTION_REQUIRED_REDIRECT
+                    )

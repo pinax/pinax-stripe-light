0ec4133ff666790d73fdaf5b803ba084a0f2dde4
Patrick Altman <paltman@gmail.com>
2015-11-27 16:22:54 -0600

    Document methods in actions.charges

--- pinax/stripe/actions/charges.py
+++ pinax/stripe/actions/charges.py
@@ -6,11 +6,38 @@ from .. import proxies
 from .. import utils
 
 
+def capture(charge, amount=None):
+    """
+    Capture the payment of an existing, uncaptured, charge.
+
+    Args:
+        charge: a pinax.stripe.proxies.ChargeProxy object
+        amount: the decimal.Decimal amount of the charge to capture
+    """
+    stripe_charge = charge.stripe_charge.capture(
+        amount=utils.convert_amount_for_api(
+            amount if amount else charge.amount,
+            charge.currency
+        )
+    )
+    sync_charge_from_stripe_data(stripe_charge)
+
+
 def create(amount, customer, source=None, currency="usd", description=None, send_receipt=True, capture=True):
     """
-    This method expects `amount` to be a Decimal type representing a
-    dollar amount. It will be converted to cents so any decimals beyond
-    two will be ignored.
+    Creates a charge for the given customer.
+
+    Args:
+        amount: should be a decimal.Decimal amount
+        customer: the Stripe id of the customer to charge
+        source: the Stripe id of the source belonging to the customer
+        currency: the currency with which to charge the amount in
+        description: a description of the charge
+        send_receipt: send a receipt upon successful charge
+        capture: immediately capture the charge instead of doing a pre-authorization
+
+    Returns:
+        a pinax.stripe.proxies.ChargeProxy object
     """
     if not isinstance(amount, decimal.Decimal):
         raise ValueError(
@@ -30,22 +57,27 @@ def create(amount, customer, source=None, currency="usd", description=None, send
     return charge
 
 
-def capture(charge, amount=None):
-    stripe_charge = charge.stripe_charge.capture(
-        amount=utils.convert_amount_for_api(
-            amount if amount else charge.amount,
-            charge.currency
-        )
-    )
-    sync_charge_from_stripe_data(stripe_charge)
-
-
 def sync_charges_for_customer(customer):
+    """
+    Populate database with all the charges for a customer.
+
+    Args:
+        customer: a pinax.stripe.proxies.CustomerProxy object
+    """
     for charge in customer.stripe_customer.charges().data:
         sync_charge_from_stripe_data(charge)
 
 
 def sync_charge_from_stripe_data(data):
+    """
+    Create or update the charge represented by the data from a Stripe API query.
+
+    Args:
+        data: the data representing a charge object in the Stripe API
+
+    Returns:
+        a pinax.stripe.proxies.ChargeProxy object
+    """
     customer = proxies.CustomerProxy.objects.get(stripe_id=data["customer"])
     obj, _ = proxies.ChargeProxy.objects.get_or_create(
         customer=customer,

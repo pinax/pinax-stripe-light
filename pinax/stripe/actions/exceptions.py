33e9797c69be3d1b0de0e6a98849fc08d0a8c097
Patrick Altman <paltman@gmail.com>
2015-11-27 19:08:40 -0600

    Add docstrings for exceptions actions

--- pinax/stripe/actions/exceptions.py
+++ pinax/stripe/actions/exceptions.py
@@ -2,4 +2,12 @@ from .. import proxies
 
 
 def log_exception(data, exception, event=None):
+    """
+    Log an exception that was captured as a result of processing events
+
+    Args:
+        data: the data to log about the exception
+        exception: the exception object itself
+        event: optionally, the event object from which the exception occurred
+    """
     proxies.EventProcessingExceptionProxy.log(data, exception, event)

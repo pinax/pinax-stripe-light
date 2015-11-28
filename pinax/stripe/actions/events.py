7c0150e5753e17a4b32943a6729d82b6d8c1361b
Patrick Altman <paltman@gmail.com>
2015-11-27 19:06:15 -0600

    Add docstrings for events actions

--- pinax/stripe/actions/events.py
+++ pinax/stripe/actions/events.py
@@ -2,11 +2,19 @@ from .. import proxies
 from ..webhooks import registry
 
 
-def dupe_event_exists(stripe_id):
-    return proxies.EventProxy.objects.filter(stripe_id=stripe_id).exists()
-
-
 def add_event(stripe_id, kind, livemode, message, api_version="", request_id="", pending_webhooks=0):
+    """
+    Adds and processes an event from a received webhook
+
+    Args:
+        stripe_id: the stripe id of the event
+        kind: the label of the event
+        livemode: True or False if the webhook was sent from livemode or not
+        message: the data of the webhook
+        api_version: the version of the Stripe API used
+        request_id: the id of the request that initiated the webhook
+        pending_webhooks: the number of pending webhooks
+    """
     event = proxies.EventProxy.objects.create(
         stripe_id=stripe_id,
         kind=kind,
@@ -20,3 +28,16 @@ def add_event(stripe_id, kind, livemode, message, api_version="", request_id="",
     if WebhookClass is not None:
         webhook = WebhookClass(event)
         webhook.process()
+
+
+def dupe_event_exists(stripe_id):
+    """
+    Checks if a duplicate event exists
+
+    Args:
+        stripe_id: the Stripe ID of the event to Checks
+
+    Returns:
+        True, if the event already exists, otherwise, False
+    """
+    return proxies.EventProxy.objects.filter(stripe_id=stripe_id).exists()

cda5536587e933097c1576a5627cf672c4468bf8
Michael Martinez <michaelmartinez@me.com>
2017-06-30 06:16:42 -0700

    Sets version to empty, defaulting to HEAD

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     ],
     install_requires=[
         "django-appconf>=1.0.1",
-        "jsonfield>=2.0.2",
+        "jsonfield",
         "stripe>=1.7.9",
         "django>=1.7",
         "pytz",

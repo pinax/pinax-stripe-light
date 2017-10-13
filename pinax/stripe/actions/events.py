c7ed716f4deefa2657bc380b6126ba05e5d991a3
Daniel Hahler <git@thequod.de>
2017-10-13 17:32:32 +0200

    Connect: fix webhook processing for connected accounts

--- pinax/stripe/actions/events.py
+++ pinax/stripe/actions/events.py
@@ -2,7 +2,8 @@ from .. import models
 from ..webhooks import registry
 
 
-def add_event(stripe_id, kind, livemode, message, api_version="", request_id="", pending_webhooks=0):
+def add_event(stripe_id, kind, livemode, message, api_version="",
+              request_id="", pending_webhooks=0, stripe_account=None):
     """
     Adds and processes an event from a received webhook
 
@@ -14,8 +15,10 @@ def add_event(stripe_id, kind, livemode, message, api_version="", request_id="",
         api_version: the version of the Stripe API used
         request_id: the id of the request that initiated the webhook
         pending_webhooks: the number of pending webhooks
+        stripe_account: the stripe_id of a Connect account
     """
     event = models.Event.objects.create(
+        stripe_account=stripe_account,
         stripe_id=stripe_id,
         kind=kind,
         livemode=livemode,
@@ -30,7 +33,7 @@ def add_event(stripe_id, kind, livemode, message, api_version="", request_id="",
         webhook.process()
 
 
-def dupe_event_exists(stripe_id):
+def dupe_event_exists(stripe_id, stripe_account=None):
     """
     Checks if a duplicate event exists
 

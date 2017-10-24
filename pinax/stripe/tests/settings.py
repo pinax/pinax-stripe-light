46e02f11757fd72480ce032ee884602c0d07a9f5
Daniel Hahler <git@thequod.de>
2017-10-24 11:57:44 +0200

    test settings: set NAME with DATABASES
    This is required when using the settings with something else than an
    in-memory SQLite DB, and when running "makemigrations --check".
    
    This uses the full set of settings from / to come with
    https://github.com/lock8/pinax-stripe/pull/22.

--- pinax/stripe/tests/settings.py
+++ pinax/stripe/tests/settings.py
@@ -1,3 +1,5 @@
+import os
+
 import django
 
 old = django.VERSION < (1, 8)
@@ -7,7 +9,10 @@ USE_TZ = True
 TIME_ZONE = "UTC"
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": os.environ.get("PINAX_STRIPE_DATABASE_ENGINE", "django.db.backends.sqlite3"),
+        "HOST": os.environ.get("PINAX_STRIPE_DATABASE_HOST", "127.0.0.1"),
+        "NAME": os.environ.get("PINAX_STRIPE_DATABASE_NAME", "pinax_stripe"),
+        "USER": os.environ.get("PINAX_STRIPE_DATABASE_USER", ""),
     }
 }
 MIDDLEWARE = [  # from 2.0 onwards, only MIDDLEWARE is used

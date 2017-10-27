50af2da4c138cb95674f0345afd3a1910ec2ddf4
Daniel Hahler <git@thequod.de>
2017-10-27 20:32:51 +0200

    fixup! actions.charges.create: commit hotfix from prod

--- pinax/stripe/tests/test_actions.py
+++ pinax/stripe/tests/test_actions.py
@@ -146,7 +146,7 @@ class ChargesTests(TestCase):
         )
         self.assertTrue(CreateMock.called)
         _, kwargs = CreateMock.call_args
-        self.assertEqual(kwargs["on_behalf_of"], "account")
+        self.assertEqual(kwargs["stripe_account"], "account")
         self.assertTrue(SyncMock.called)
         self.assertTrue(SendReceiptMock.called)
 

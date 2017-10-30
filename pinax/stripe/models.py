7bee4732ac7eb9ffd7f0913872bada31d635bd53
Daniel Hahler <git@thequod.de>
2017-10-30 19:40:51 +0100

    Revert "Merge pull request #25 from blueyed/revert-stripe_publishable_key"
    This reverts commit 7881f7293dea472da7ac103ac416ad81b1ada8dd, reversing
    changes made to f0172f1cec23cb45c73f2436ebcad32fbe622239.

--- pinax/stripe/models.py
+++ pinax/stripe/models.py
@@ -552,6 +552,8 @@ class Account(StripeObject):
 
     stripe_publishable_key = models.CharField(null=True, blank=True, max_length=100)
 
+    stripe_publishable_key = models.CharField(null=True, blank=True, max_length=100)
+
     product_description = models.TextField(null=True, blank=True)
     statement_descriptor = models.TextField(null=True, blank=True)
     support_email = models.TextField(null=True, blank=True)

2c521b418c09b76c5893772b0db094ab21090509
Daniel Hahler <git@thequod.de>
2017-11-17 01:41:26 +0100

    account.application.deauthorized: remove ValueError
    The account might still be accessible, e.g. if it was connected multiple
    times.

--- pinax/stripe/webhooks.py
+++ pinax/stripe/webhooks.py
@@ -154,12 +154,11 @@ class AccountApplicationDeauthorizeWebhook(Webhook):
 
         When this event is for a connected account we should not be able to
         fetch the event anymore (since we have been disconnected).
+        But there might be multiple connections (e.g. for Dev/Prod).
 
-        Therefore we try to retrieve the event:
-         - in case of PermissionError exception, everything is perfectly normal.
-           It means the account has been deauthorized.
-         - In case no exception has been caught, then, most likely, the event has been forged
-           to make you believe the account has been disabled despite it is still functioning.
+        Therefore we try to retrieve the event, and handle a
+        PermissionError exception to be expected (since we cannot access the
+        account anymore).
         """
         try:
             super(AccountApplicationDeauthorizeWebhook, self).validate()
@@ -168,11 +167,8 @@ class AccountApplicationDeauthorizeWebhook(Webhook):
                 stripe_account_id = self.stripe_account.stripe_id
                 if not(stripe_account_id in str(exc) and obfuscate_secret_key(settings.PINAX_STRIPE_SECRET_KEY) in str(exc)):
                     raise exc
-        else:
-            if self.stripe_account:
-                raise ValueError("The remote account is still valid. This might be a hostile event")
-        self.event.valid = True
-        self.event.validated_message = self.event.webhook_message
+            self.event.valid = True
+            self.event.validated_message = self.event.webhook_message
 
     def process_webhook(self):
         if self.stripe_account is not None:

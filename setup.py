bb25a054eafaeaf08397048d43e5520ef26f8e0c
Patrick Altman <paltman@gmail.com>
2018-08-04 08:30:38 -0500

    Pin stripe to <2.0

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
     install_requires=[
         "django-appconf>=1.0.1",
         "jsonfield>=1.0.3",
-        "stripe>=1.7.9",
+        "stripe>=1.7.9, <2.0",
         "django>=1.8",
         "pytz",
         "six",

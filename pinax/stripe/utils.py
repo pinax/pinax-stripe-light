766ed1e8d07619eac9d4db2ab3d0d49a3d3c5762
Jim Simon <jksimoniii@gmail.com>
2019-02-01 13:50:15 -0500

    Handle null Amounts in convert_amount_for_db

--- pinax/stripe/utils.py
+++ pinax/stripe/utils.py
@@ -33,6 +33,8 @@ ZERO_DECIMAL_CURRENCIES = [
 def convert_amount_for_db(amount, currency="usd"):
     if currency is None:  # @@@ not sure if this is right; find out what we should do when API returns null for currency
         currency = "usd"
+    if amount is None:    # @@@ not sure if this is right; find out what we should do when API returns null for amount
+        amount = 0
     return (amount / decimal.Decimal("100")) if currency.lower() not in ZERO_DECIMAL_CURRENCIES else decimal.Decimal(amount)
 
 

ff3044517d7d92deb4642bf6b3025a85fe76c56f
iarp <iarp.opensource@gmail.com>
2020-08-07 16:12:31 -0400

    correcting lint failure too many blank lines

--- pinax/stripe/tests/test_webhooks.py
+++ pinax/stripe/tests/test_webhooks.py
@@ -262,7 +262,6 @@ class CustomerDeletedWebhookTest(TestCase):
         self.assertIsNone(customer.user)
 
 
-
 class ChargeDisputeClosedWebhookTest(TestCase):
 
     @patch("stripe.Charge.retrieve")
